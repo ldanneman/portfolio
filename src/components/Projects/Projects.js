edbba78c7931a6651d12e82b6a2784f1de734025
Lev Danneman <levdanneman@gmail.com>
2022-08-30 11:54:20 -0400

    initial

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
+
+const Projects = () => (
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map((project) => (
+        <BlogCard key={project.id}>
+          <Img src={project.image} />
+          <TitleContent>
+            <HeaderThree title>{project.title}</HeaderThree>
+            <Hr />
+          </TitleContent>
+          <CardInfo>{project.description}</CardInfo>
+         <div>
+          <TitleContent>Stack</TitleContent>
+          <TagList></TagList>
+         </div>
+        </BlogCard>
+      ))}
+    </GridContainer>
+  </Section>
+);
+
+export default Projects;
